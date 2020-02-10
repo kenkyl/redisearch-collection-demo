66236dda5f675f9e8cb28e35b7e1950675938f52
Kyle Kennedy <kylekennedy@Kyles-MBP.attlocal.net>
2020-02-10 17:12:42 -0600

    create index working

--- initiate-collection-data.py
+++ initiate-collection-data.py
@@ -1,5 +1,6 @@
 import redis
 import redisearch
+import sys
 
 # REDIS CONSTANTS
 redis_host = 'localhost'
@@ -39,11 +40,22 @@ def main():
 
     try:
         rs.create_index((
-            redisearch.TextField('title'),
-            redisearch.TextField('artist'),
-            redisearch.NumericField('year'),
-            redisearch.
+            redisearch.TextField('title', sortable=True),   
+            redisearch.TextField('artist', sortable=True),
+            redisearch.NumericField('year', sortable=True),
+            redisearch.TagField('genre', separator=',')
         ))
+    except Exception:
+            print(f'Error creating index: {sys.exc_info()}')
+    print(f'index info: {rs.info()}')
+
+    test = True
+
+    while test:
+        txt = input("test me and enter something here: ")
+        if (txt == "quit"):
+            test = False
+
 
 if __name__ == '__main__':
     main()
